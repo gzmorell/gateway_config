a1e7f2f321023c55638aea39b8cb0e701e7a0d93
Francisco Gonzalez <fgonzalez@seairtech.com>
2024-11-10 18:34:05 +0100

    Reboot command

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@
 #include "ftxui/screen/color.hpp" // for Color, Color::BlueLight, Color::RedLight, Color::Black, Color::Blue, Color::Cyan, Color::CyanLight, Color::GrayDark, Color::GrayLight, Color::Green, Color::GreenLight, Color::Magenta, Color::MagentaLight, Color::Red, Color::White, Color::Yellow, Color::YellowLight, Color::Default, Color::Palette256, ftxui
 // #include "ftxui/screen/color_info.hpp"  // for ColorInfo
 // #include "ftxui/screen/terminal.hpp"    // for Size, Dimensions
+#include "subprocess.hpp"
 
 using namespace ftxui;
 
@@ -208,8 +209,11 @@ int main()
     int shift = 0;
     bool shown_exit = false;
     bool shown_reboot = false;
-    auto run_exit = screen.ExitLoopClosure();
-    auto run_reboot = screen.ExitLoopClosure();
+    auto run_exit = [&] {screen.Exit();};
+    auto run_reboot = [&] {
+        subprocess::command cmd{"sudo reboot"};
+        cmd.run();
+    };
     auto cancel_exit = [&] { shown_exit = false; };
     auto show_exit = [&] { shown_exit = true; };
     auto cancel_reboot = [&] { shown_reboot = false; };
